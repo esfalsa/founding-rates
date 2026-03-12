607743c801a06476ec32f0a234b5e787348c9eed
Pronoun <pronoun.nationstates@gmail.com>
2026-03-12 02:56:28 -0700

    fix: toggle Tailwind dark mode by class

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-// import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://esfalsa.github.io",
-
-  // integrations: [tailwind({ applyBaseStyles: false })],
   base: "/founding-rates",
-
   vite: {
     plugins: [tailwindcss()],
   },
